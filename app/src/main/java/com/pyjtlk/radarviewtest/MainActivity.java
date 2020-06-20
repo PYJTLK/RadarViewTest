6f441c8c9ee9d43455ba126839b4f558610e22ef
Jimmy <742813255@qq.com>
2020-06-20 18:13:39 +0800

    修复初次设值不显示的bug，重写演示代码

--- app/src/main/java/com/pyjtlk/radarviewtest/MainActivity.java
+++ app/src/main/java/com/pyjtlk/radarviewtest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pyjtlk.radarviewtest;
 
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,13 +18,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         radarChart = findViewById(R.id.radarChart);
+
+        //布局文件上设置attributeCount=6，则需要6个参数
+        //attributeLevel=6，则各参数的取值范围为0-6
         int attrs[] = {6,1,2,3,4,5};
         radarChart.setAttributes(attrs);
         radarChart.setAttribute(0,1);
     }
 
     public void onClicked(View view) {
-        int attrs[] = {4,5,6,7,1,2};
-        radarChart.setAttributes(attrs);
+        radarChart.setAttribute(0,1);
+        radarChart.setAttribute(1,4);
+        radarChart.setAttribute(2,2);
+    }
+
+    public void onClicked2(View view) {
+        radarChart.setAttribute(0,6);
+        radarChart.setAttribute(1,2);
+        radarChart.setAttribute(2,3);
     }
 }
--- radarview/src/main/java/com/pyjtlk/radarview/RadarChart.java
+++ radarview/src/main/java/com/pyjtlk/radarview/RadarChart.java
@@ -14,7 +14,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
-
 import java.util.ArrayList;
 
 /**
@@ -424,6 +423,7 @@ public class RadarChart extends View{
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         mAttrAnimator.cancel();
+        mAttrAnimator.removeAllUpdateListeners();
     }
 
     /**
